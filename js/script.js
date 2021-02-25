353601146c2d89189505ac82f5b84b029fc82b5d
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-02-25 14:45:44 +0100

    inseriti prompt

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+var nome = 'Inserisci il tuo nome';
+var cognome = 'Inserisci il tuo cognome';
+var colore = 'Il tuo colore preferito?';
+prompt(nome) + prompt(cognome) + prompt(colore);
+document.getElementById('name').innerHTML = nome + cognome + colore;
